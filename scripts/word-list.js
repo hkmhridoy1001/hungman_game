0819823159094b0803571367bf175263f5538644
Hridoy Mohonto <112055398+hkmhridoy1001@users.noreply.github.com>
2023-08-10 00:40:10 +0600

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,170 @@
+const wordList = [
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings."
+    },
+    {
+        word: "piano",
+        hint: "A keyboard instrument with black and white keys."
+    },
+    {
+        word: "violin",
+        hint: "A small stringed instrument played with a bow."
+    },
+    {
+        word: "trumpet",
+        hint: "A brass instrument often used in bands and orchestras."
+    },
+    {
+        word: "flute",
+        hint: "A woodwind instrument played by blowing across a hole."
+    },
+    {
+        word: "drum",
+        hint: "A percussion instrument played by striking with sticks or hands."
+    },
+    {
+        word: "saxophone",
+        hint: "A curved wind instrument with a single reed mouthpiece."
+    },
+    {
+        word: "clarinet",
+        hint: "A woodwind instrument with a slender body and a reed mouthpiece."
+    },
+    {
+        word: "cello",
+        hint: "A large stringed instrument held between the knees when played."
+    },
+    {
+        word: "harp",
+        hint: "A stringed instrument with a triangular frame, played by plucking."
+    },
+    {
+        word: "trombone",
+        hint: "A brass instrument with a sliding U-shaped tube."
+    },
+    {
+        word: "accordion",
+        hint: "A portable instrument with a bellows and buttons or keys."
+    },
+    {
+        word: "xylophone",
+        hint: "A percussion instrument with wooden bars of different lengths."
+    },
+    {
+        word: "bagpipes",
+        hint: "A wind instrument with pipes and a bag, often associated with Scotland."
+    },
+    {
+        word: "ukulele",
+        hint: "A small guitar-like instrument with four strings."
+    },
+    {
+        word: "organ",
+        hint: "A keyboard instrument with pipes that produce sound."
+    },
+    {
+        word: "bassoon",
+        hint: "A large woodwind instrument with a double reed."
+    },
+    {
+        word: "bongo",
+        hint: "A pair of small, open-bottomed drums played with hands."
+    },
+    {
+        word: "mandolin",
+        hint: "A small stringed instrument with a rounded back."
+    },
+    {
+        word: "piccolo",
+        hint: "A small, high-pitched flute."
+    },
+    {
+        word: "oboe",
+        hint: "A woodwind instrument with a double reed."
+    },
+    {
+        word: "trumpet",
+        hint: "A brass instrument often used in bands and orchestras."
+    },
+    {
+        word: "flute",
+        hint: "A woodwind instrument played by blowing across a hole."
+    },
+    {
+        word: "drum",
+        hint: "A percussion instrument played by striking with sticks or hands."
+    },
+    {
+        word: "saxophone",
+        hint: "A curved wind instrument with a single reed mouthpiece."
+    },
+    {
+        word: "clarinet",
+        hint: "A woodwind instrument with a slender body and a reed mouthpiece."
+    },
+    {
+        word: "cello",
+        hint: "A large stringed instrument held between the knees when played."
+    },
+    {
+        word: "harp",
+        hint: "A stringed instrument with a triangular frame, played by plucking."
+    },
+    {
+        word: "trombone",
+        hint: "A brass instrument with a sliding U-shaped tube."
+    },
+    {
+        word: "xylophone",
+        hint: "A percussion instrument with wooden bars of different lengths."
+    },
+    {
+        word: "bagpipes",
+        hint: "A wind instrument with pipes and a bag, often associated with Scotland."
+    },
+    {
+        word: "ukulele",
+        hint: "A small guitar-like instrument with four strings."
+    },
+    {
+        word: "organ",
+        hint: "A keyboard instrument with pipes that produce sound."
+    },
+    {
+        word: "bassoon",
+        hint: "A large woodwind instrument with a double reed."
+    },
+    {
+        word: "bongo",
+        hint: "A pair of small, open-bottomed drums played with hands."
+    },
+    {
+        word: "mandolin",
+        hint: "A small stringed instrument with a rounded back."
+    },
+    {
+        word: "piccolo",
+        hint: "A small, high-pitched flute."
+    },
+    {
+        word: "oboe",
+        hint: "A woodwind instrument with a double reed."
+    },
+    {
+        word: "triangle",
+        hint: "A percussion instrument in the shape of a triangle."
+    },
+    {
+        word: "recorder",
+        hint: "A woodwind instrument with finger holes and a fipple mouthpiece."
+    },
+    {
+        word: "sitar",
+        hint: "A stringed instrument with a long neck and resonating gourd."
+    },
+    {
+        word: "tambourine",
+        hint: "A circular percussion instrument with jingling metal discs."
+    }
+]
